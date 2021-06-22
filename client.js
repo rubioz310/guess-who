556e8cf378ff526824639585ac49a9d63eb9519c
Jose Rubio <ru310z@gmail.com>
2021-06-21 19:21:30 -0500

    App now shows random name from array on top of DOM

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 console.log('Here are all the available people:', people);
 $(readyNow);
 function readyNow(){
+    updateName();
     showPictures();
     $('#gameSection').on('click', 'img', function(){
         showName($(this).data('name'));
@@ -19,3 +20,13 @@ function showName(name){
     console.log(name);
 }
 
+function updateName(){
+    let randomName = people[randomNumber(0, people.length - 1)];
+    $('#personName').text(`Click on: ${randomName.name}`);
+}
+
+
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
